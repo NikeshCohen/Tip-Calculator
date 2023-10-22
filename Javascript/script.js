b5e63c5bb0a418c19ddc088a6fb009932a805a32
Nikesh Cohen <you@example.com>
2023-10-22 18:31:55 +0200

    Update Javascript

--- Javascript/script.js
+++ Javascript/script.js
@@ -6,12 +6,54 @@ const billAmount = document.querySelector(".bill-amount");
 const tipPercentageBtnAll = document.querySelectorAll(".tip-percentage-btn");
 const tipPercentageInput = document.querySelector(".tip-percentage-input");
 const numPeople = document.querySelector(".no-people");
+const calculate = document.querySelector(".calculator-btn");
 const reset = document.querySelector(".reset-btn");
 
-///////// DOM ELEMENTS /////////
+///////// FILL IN /////////
 
-tipPercentageBtnAll.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    console.log(btn.value);
-  });
+let bill = billAmount.value;
+
+const findTip = () => {
+  let tip;
+
+  if (tipPercentageInput.value) {
+    tip = tipPercentageInput.value;
+    console.log(tip);
+  } else {
+    tipPercentageBtnAll.forEach((btn) => {
+      btn.addEventListener("click", function () {
+        tipPercentageBtnAll.forEach((el) => {
+          if (el.classList.contains("active")) {
+            el.classList.remove("active");
+          } else if (el === btn) {
+            el.classList.add("active");
+          }
+        });
+      });
+    });
+  }
+};
+
+// function findTip() {
+//   tipPercentageBtnAll.forEach((btn) => {
+//     btn.addEventListener("click", function () {
+//       tipPercentageBtnAll.forEach((el) => {
+//         if (el.classList.contains("active")) {
+//           el.classList.remove("active");
+//         } else if (el === btn) {
+//           el.classList.add("active");
+//         }
+//       });
+
+//       // Move the console.log statement inside the callback
+//       console.log(el.value);
+//       tip = el.value;
+//     });
+//   });
+// }
+
+calculate.addEventListener("click", () => {
+  findTip();
 });
+
+findTip();
